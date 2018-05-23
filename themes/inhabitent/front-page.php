fe6dc6f2d215323bcfb55cdd417c42b5dd5adf3c
Pamela Chong <pjchong91@gmail.com>
2018-05-23 12:42:18 -0700

    Dynamically loaded Shop Stuff and Journal

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -19,11 +19,31 @@ get_header(''); ?>
         <section class="container-holder">
             <div class="container">
                 <h1 class="container-header">Shop Stuff</h1>
-                <div class="shop-stuff listed-posts">
-                    <div class="ssbox"></div>
-                    <div class="ssbox"></div>
-                    <div class="ssbox"></div>
-                    <div class="ssbox"></div>
+             
+<?php
+                $terms = get_terms( array(
+                    'taxonomy' => 'product_type',
+                    'hide_empty' => 0,
+                ));
+
+                if ( ! empty( $terms ) && ! is_wp_error( $terms ) ) : ?>
+                   <div class="shop-stuff listed-posts">
+                <!-- <div class="product-type-blocks"> -->
+                    <?php foreach ( $terms as $term ) : ?>
+                    <div class="ssbox">
+                        <img src="<?php echo get_template_directory_uri() . './assets/images/product-type-icons/' . $term->slug; ?>.svg"
+                        alt="<?php echo $term->name; ?>"/>
+
+                        <p><?php echo $term->description; ?></p>
+
+                        <p>
+                            <a href="<?php echo get_term_link ( $term ); ?>" class="green-button">
+                            <?php echo $term->name; ?> Stuff</a>
+         
+            </p>
+            </div>
+            <?php endforeach; ?>
+            <?php endif; ?>
                 </div> <!--end of shop stuff-->
             </div> <!--end of container-->
         </section>
@@ -34,11 +54,31 @@ get_header(''); ?>
             <div class="container">
                 <h1 class="container-header">Inhabitent Journal</h1>
                 <div class="inhabitent-journal listed-posts">
-                    <div class="ijbox"></div>
-                    <div class="ijbox"></div>
-                    <div class="ijbox"></div>
-                </div>  
-            </div>
+
+                <?php
+   $args = array( 'post_type' => 'post', 'order' => 'DSC', 'numberposts' => '3' );
+   $journal_posts = get_posts( $args ); // returns an array of posts
+?>
+
+<?php foreach ( $journal_posts as $post ) : setup_postdata( $post ); ?>
+<div class="ijbox">
+    <header class="entry-header">
+		<?php if ( has_post_thumbnail() ) : ?>
+			<?php the_post_thumbnail( 'large' ); ?>
+        <?php endif; ?>
+    </header>
+
+    <div class="entry-meta">
+        <?php red_starter_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?>  
+        <?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+    <a href="<?php echo esc_url(get_permalink())?>" class="button-border">Read Entry </a>
+    </div>
+</div>
+
+    <?php endforeach; wp_reset_postdata(); ?>
+</div> <!--end of inhabitent-journal-->
+
+            </div><!--end of container-->
         </section>
 
         <section class="container-holder">
